714e392127e30a6e4316023bc56ffb90a52f5168
alexfallaw <96636767+alexfallaw@users.noreply.github.com>
2022-12-11 18:20:22 -0600

    initial commit

---
+++ src/data/piece-list.js
@@ -0,0 +1,33 @@
+import Piece from "./piece";
+
+const PieceList = new Map();
+
+PieceList.set(1, new Piece(
+    "Neon Hands",
+    "2020",
+    "Digital",
+    "500 x 500 px",
+    "This is a doodle of two hands holding hands",
+    "Blah blah blah blah blah blah blah blah",
+    "images/jpg/neon_hands.jpg",
+    "image/png/neon_hands.png"));
+PieceList.set(2, new Piece(
+    "Buttercup",
+    "2020",
+    "Digital",
+    "400 x 600 px",
+    "This is a drawing of some guy",
+    "Blah blah blah blah blah blah blah blah",
+    "images/jpg/buttercup_love.jpg",
+    "image/png/buttercup_love.png"));
+PieceList.set(3, new Piece(
+    "Rose Skull",
+    "2019",
+    "Digital",
+    "500 x 500 px",
+    "Personal project",
+    "Blah blah blah blah blah blah blah blah",
+    "images/jpg/rose_skull.jpg",
+    "image/png/rose_skull.png"));
+
+export default PieceList;
---
+++ src/data/piece.js
@@ -0,0 +1,23 @@
+class Piece {
+    constructor(
+      title,
+      year,
+      medium,
+      dimensions,
+      shortBlurb,
+      longBlurb,
+      smallPath,
+      largePath,
+    ) {
+      this.title = title;
+      this.year = year;
+      this.medium = medium;
+      this.dimensions = dimensions;
+      this.shortBlurb = shortBlurb;
+      this.longBlurb = longBlurb;
+      this.smallPath = smallPath;
+      this.largePath = largePath;
+    }
+  }
+  
+  export default Piece;
