5c78200080fb59392ce59fd6c8107501a45389fc
alexfallaw <96636767+alexfallaw@users.noreply.github.com>
2022-12-14 15:19:54 -0600

    back button
    added back button to header bar

--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -2,14 +2,17 @@ import "../styles/header-bar.css";
 import "../styles/common.css";
 import { Menu } from "./menu";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export function HeaderBar() {
+  let navigate = useNavigate();
   return (
     <div className="head">
       <div id="head-bar">
-        <div id="back" className="head-icon">
+        <button onClick={() => navigate(-1)} id="back" className="head-icon">
           <img src="images/icons/back-arrow.svg" alt="" className="head-img" />
-        </div>
+        </button>
+
         <Link to="/" id="logo" className="head-icon">
           <img src="images/icons/logo_light.svg" alt="" className="head-img" />
         </Link>
